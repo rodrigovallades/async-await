2a517327c82bd5d8687279185a9d2322435c1261
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-11-06 13:59:55 -0200

    ♻️ Refactor concurrent promises with Promise.all

--- async.js
+++ async.js
@@ -3,28 +3,17 @@ const fetch = require('node-fetch')
 async function fetchFromGitHub(endpoint) {
 	const url = `https://api.github.com${endpoint}`;
 	const response = await fetch(url);
-  const body = await response.json();
-
-  if (response.status !== 200)
-    throw Error(body.message)
-
-  return body;
+  return await response.json();
 }
 
 async function showUserAndRepos(handle) {
-  try {
-    const userPromise = fetchFromGitHub(`/users/${handle}`);
-    const reposPromise = fetchFromGitHub(`/users/${handle}/repos`);
+  const [user, repos] = await Promise.all([
+    fetchFromGitHub(`/users/${handle}`),
+    fetchFromGitHub(`/users/${handle}/repos`)
+  ])
 
-    const user = await userPromise;
-    const repos = await reposPromise;
-
-    console.log(user.name);
-    console.log(`${repos.length} repos`);
-  } catch (err) {
-    console.log(`Error: ${err.message}`);
-
-  }
+  console.log(user.name);
+  console.log(`${repos.length} repos`);
 }
 
 showUserAndRepos('rodrigovallades');
