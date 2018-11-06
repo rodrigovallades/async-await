d2215f8e003029311638679bb67fa1961628ff0a
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-11-06 11:43:53 -0200

    ♻️ Convert IIFE into Async

--- async.js
+++ async.js
@@ -1,15 +1,16 @@
 const fetch = require('node-fetch')
 
-async function showGitHubUser(handle) {
-	const url = `https://api.github.com/users/${handle}`;
-	const response = await fetch(url);
-	return await response.json();
+class GitHubApiClient {
+  async fetchUser(handle) {
+    const url = `https://api.github.com/users/${handle}`;
+    const response = await fetch(url);
+    return await response.json();
+  }
 }
 
-showGitHubUser('rodrigovallades')
-  .then(user => {
-    console.log(user);
-
-    console.log(user.name);
-    console.log(user.locatoin);
-  });
+(async () => {
+  const client = new GitHubApiClient();
+  const user = await client.fetchUser('rodrigovallades')
+  console.log(user.name);
+  console.log(user.location);
+})()
