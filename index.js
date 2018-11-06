d37eef95abb7c4590eb1f48aea5c0fd1d932c797
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-11-06 11:23:13 -0200

    ♻️ Refactor to async/await

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const fetch = require('node-fetch')
 
-function showGitHubUser(handle) {
-  const url = `https://api.github.com/users/${handle}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(user => {
-      console.log(user.name);
-      console.log(user.location);
-    })
+async function showGitHubUser(handle) {
+	const url = `https://api.github.com/users/${handle}`;
+	const response = await fetch(url);
+	const user = await response.json();
+	console.log(user.name);
+	console.log(user.location);
 }
 
 showGitHubUser('rodrigovallades')
