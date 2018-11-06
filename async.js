9193d462189b3f5735e433eebb9305749b4497a7
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-11-06 11:31:19 -0200

    ♻️ Call an Asynchronous Function in a Promise Chain

--- async.js
+++ async.js
@@ -3,9 +3,13 @@ const fetch = require('node-fetch')
 async function showGitHubUser(handle) {
 	const url = `https://api.github.com/users/${handle}`;
 	const response = await fetch(url);
-	const user = await response.json();
-	console.log(user.name);
-	console.log(user.location);
+	return await response.json();
 }
 
 showGitHubUser('rodrigovallades')
+  .then(user => {
+    console.log(user);
+
+    console.log(user.name);
+    console.log(user.locatoin);
+  });
